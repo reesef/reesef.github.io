b3f87c89dd8008c80b32ab6500126e1b91d41b44
Filip Reese <Filip@Filips-MacBook-Pro.local>
2016-02-02 14:02:47 +1100

    Add more projects stuff

--- js/projects.js
+++ js/projects.js
@@ -1,8 +1,34 @@
 var source   = $("#projects-template").html();
 var template = Handlebars.compile(source);
 var data = { projects: [
-    {name: "alan", image: "Alan", description: "Johnson" },
-    {name: "alan", image: "Alan", description: "Johnson" },
-    {name: "alan", image: "Alan", description: "Johnson" }
-  ]};
+
+    { name: "Browse contests - 99designs",
+      image: "images/projects/browse.png",
+      description: "kdjflksd sdklfklsfj k" },
+
+    { name: "Responsive contests - 99designs",
+      image: "images/projects/contest-page.png",
+      description: "Johnson sdkfjs skldfj" },
+
+    { name: "Fullscreen design viewer - 99designs",
+      image: "images/projects/------",
+      description: "Johnson sdkfjs skldfj" },
+
+    { name: "Messaging - 99designs",
+      image: "images/projects/------",
+      description: "Johnson sdkfjs skldfj" },
+
+    { name: "Niño",
+      image: "images/projects/------",
+      description: "Johnson sdkfjs skldfj" },
+
+    { name: "Landscaping.io",
+      image: "images/projects/------",
+      description: "Johnson sdkfjs skldfj" },
+
+    { name: "Totoapp.com",
+      image: "images/projects/------",
+      description: "Johnson sdkfjs skldfj" }
+]};
+
 $("#projects").html(template(data));
